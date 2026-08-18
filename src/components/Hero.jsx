d12b5993e839ca08e7695d94c7631bfdd5e2e7bf
Sushmita Dasari <147468660+Sushmitadasari@users.noreply.github.com>
2026-08-18 13:09:26 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 // Adjusted import path for the video
-import heroVideo from '../assets/hero video/herovideo.mp4';
+import heroVideo from '../assets/hero_video/hero_video.mp4';
 
 const Hero = () => {
   const videoRef = useRef(null);
